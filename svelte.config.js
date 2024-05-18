b1cfb894d4f0a9505b7dfa870d6e495481860739
Jay Cho <jaycho1214@gmail.com>
2024-05-18 14:15:03 +0900

    migrate to github pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,6 +12,9 @@ const config = {
 			fallback: '404.html',
 			strict: true,
 		}),
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/website' : '',
+		},
 	},
 };
 
--- terraform/gcp_backend.tf
+++
@@ -1,5 +0,0 @@
-resource "google_compute_backend_bucket" "site_backend" {
-  name        = "crosswand-website-backend"
-  bucket_name = google_storage_bucket.static_site.name
-  enable_cdn  = true
-}
